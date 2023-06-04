02f61b91a416b25ec299cfa81cf8c1df1880a8eb
AgustinFZ2 <agusfalconz22@gmail.com>
2023-06-04 13:57:21 -0300

    c9

--- carruusell.js
+++ carruusell.js
@@ -1,16 +1,16 @@
-  const carousel = document.querySelector('.carousel-inner');
-    const prevBtn = document.getElementById('prevBtn');
-    const nextBtn = document.getElementById('nextBtn');
-    const carouselItems = document.querySelectorAll('.carousel-item');
-    const itemWidth = carouselItems[0].clientWidth;
-    let counter = 0;
-    
-    prevBtn.addEventListener('click', () => {
-      counter--;
-      carousel.style.transform = `translateX(-${counter * itemWidth}px)`;
-    });
-    
-    nextBtn.addEventListener('click', () => {
-      counter++;
-      carousel.style.transform = `translateX(-${counter * itemWidth}px)`;
-    });
+ const carousel = document.querySelector('.carousel-inner');
+ const prevBtn = document.getElementById('prevBtn');
+ const nextBtn = document.getElementById('nextBtn');
+ const carouselItems = document.querySelectorAll('.carousel-item');
+ const itemWidth = carouselItems[0].clientWidth;
+ let counter = 0;
+ 
+prevBtn.addEventListener('click', () => {
+ counter--;
+ carousel.style.transform = `translateX(-${counter * itemWidth}px)`;
+ });
+ 
+nextBtn.addEventListener('click', () => {
+ counter++;
+ carousel.style.transform = `translateX(-${counter * itemWidth}px)`;
+ });
