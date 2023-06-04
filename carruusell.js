73ed30a1bf021fca6ca79f0c4cde3b8246c52b85
AgustinFZ2 <agusfalconz22@gmail.com>
2023-06-04 16:39:00 -0300

    jajaja

--- carruusell.js
+++ carruusell.js
@@ -1,34 +1,24 @@
- const carousel = document.querySelector('.carousel-inner');
- const prevBtn = document.getElementById('prevBtn');
- const nextBtn = document.getElementById('nextBtn');
- const carouselItems = document.querySelectorAll('.carousel-item');
- const itemWidth = carouselItems[0].clientWidth;
- let counter = 0;
- 
-prevBtn.addEventListener('click', () => {
- counter--;
- carousel.style.transform = `translateX(-${counter * itemWidth}px)`;
- });
- 
-nextBtn.addEventListener('click', () => {
- counter++;
- carousel.style.transform = `translateX(-${counter * itemWidth}px)`;
- });
- /**/
- document.addEventListener("DOMContentLoaded", function() {
-    var carouselContainer = document.querySelector(".carousel-container");
-    var carouselImages = document.querySelectorAll(".carousel-container img");
+document.addEventListener("DOMContentLoaded", function() {
+    const carousel = document.querySelector('.carousel-inner');
+    const prevBtn = document.getElementById('prevBtn');
+    const nextBtn = document.getElementById('nextBtn');
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    const itemWidth = carouselItems[0].clientWidth;
+    let counter = 0;
   
-    var imageWidth = carouselImages[0].clientWidth;
-    var currentIndex = 0;
+    prevBtn.addEventListener('click', () => {
+      counter--;
+      carousel.style.transform = `translateX(-${counter * itemWidth}px)`;
+    });
   
-    function slideTo(index) {
-      carouselContainer.style.transform = "translateX(-" + (imageWidth * index) + "px)";
-      currentIndex = index;
-    }
+    nextBtn.addEventListener('click', () => {
+      counter++;
+      carousel.style.transform = `translateX(-${counter * itemWidth}px)`;
+    });
   
     setInterval(function() {
-      var nextIndex = (currentIndex + 1) % carouselImages.length;
-      slideTo(nextIndex);
+      counter++;
+      carousel.style.transform = `translateX(-${counter * itemWidth}px)`;
     }, 3000);
   });
+  
