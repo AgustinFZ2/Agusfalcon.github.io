960fa0a1f8a3775e7fc23fde299c7b83a11268d3
AgustinFZ2 <agusfalconz22@gmail.com>
2023-06-04 18:15:59 -0300

    zzz

--- carruusell.js
+++ carruusell.js
@@ -1,3 +1,58 @@
+var index = 0; 
+var locations = ["Amsterdam, The Netherlands", "Sydney, Australia", "San Francisco, California"]
+
+var slides = document.getElementsByClassName("slides"); 
+var nextArrow = document.getElementById("next");
+
+var previousArrow = document.getElementById("previous");
+
+var place = document.getElementById("place");
+
+var dotsContainer = document.getElementById("dotsContainer");
+
+var dotArray = document.getElementsByClassName("dots"); 
+
+createDots(); 
+showSlides(index); 
+
+function createDots() {
+	for (i=0; i<slides.length; i++) {
+	var dot = document.createElement("span");
+	dot.className = "dots"; 
+	dotsContainer.appendChild(dot); 
+	}
+}
+
+function showSlides(x) {
+	if (x > slides.length-1) {
+		index = 0; 
+	}
+	if (x < 0) {
+		index = slides.length-1; 
+	}
+	for (i=0; i < slides.length; i++) {
+		slides[i].style.display = "none"; 
+		dotArray[i].className = "dots";  
+	}
+	
+	slides[index].style.display = "block";
+	dotArray[index].className += " activeDot"; 
+	place.innerHTML = locations[index]; 
+}
+
+nextArrow.onclick = function() {
+	index += 1; 
+	showSlides(index); 
+} 
+
+previousArrow.onclick = function() {
+	index -= 1; 
+	showSlides(index); 
+} 
+
+dotArray[0].onclick = showSlides(1); 
+
+/*
 document.addEventListener("DOMContentLoaded", function() {
     const carousel = document.querySelector('.carousel-inner');
     const prevBtn = document.getElementById('prevBtn');
@@ -21,4 +76,4 @@ document.addEventListener("DOMContentLoaded", function() {
       carousel.style.transform = `translateX(-${counter * itemWidth}px)`;
     }, 3000);
   });
-  
+  */
