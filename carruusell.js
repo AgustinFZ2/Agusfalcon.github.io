40379e7ea77e8d2f2b1d17e7757bbc6cd80293bd
AgustinFZ2 <agusfalconz22@gmail.com>
2023-06-04 12:13:14 -0300

    kkk

--- carruusell.js
+++ carruusell.js
@@ -1,10 +1,16 @@
-$('#myCarouselCustom').carousel();
-
-// Go to the previous item
-$("#prevBtn").click(function(){
-    $("#myCarouselCustom").carousel("prev");
-});
-// Go to the previous item
-$("#nextBtn").click(function(){
-    $("#myCarouselCustom").carousel("next");
-});
+  const carousel = document.querySelector('.carousel-inner');
+    const prevBtn = document.getElementById('prevBtn');
+    const nextBtn = document.getElementById('nextBtn');
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    const itemWidth = carouselItems[0].clientWidth;
+    let counter = 0;
+    
+    prevBtn.addEventListener('click', () => {
+      counter--;
+      carousel.style.transform = `translateX(-${counter * itemWidth}px)`;
+    });
+    
+    nextBtn.addEventListener('click', () => {
+      counter++;
+      carousel.style.transform = `translateX(-${counter * itemWidth}px)`;
+    });
