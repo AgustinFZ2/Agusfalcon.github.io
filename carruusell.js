d5a4ee921fd031efd09383b54c9b599d9a134678
AgustinFZ2 <agusfalconz22@gmail.com>
2023-06-04 16:28:10 -0300

    f0

--- carruusell.js
+++ carruusell.js
@@ -14,3 +14,21 @@ nextBtn.addEventListener('click', () => {
  counter++;
  carousel.style.transform = `translateX(-${counter * itemWidth}px)`;
  });
+ /**/
+ document.addEventListener("DOMContentLoaded", function() {
+    var carouselContainer = document.querySelector(".carousel-container");
+    var carouselImages = document.querySelectorAll(".carousel-container img");
+  
+    var imageWidth = carouselImages[0].clientWidth;
+    var currentIndex = 0;
+  
+    function slideTo(index) {
+      carouselContainer.style.transform = "translateX(-" + (imageWidth * index) + "px)";
+      currentIndex = index;
+    }
+  
+    setInterval(function() {
+      var nextIndex = (currentIndex + 1) % carouselImages.length;
+      slideTo(nextIndex);
+    }, 3000);
+  });
